7a314fa4f500568cd019a06783bf8f5f99a683cf
zyh <2508939001@qq.com>
2022-08-31 10:33:55 +0800

    Start

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book0',
+    'book0.apps.Book0Config',
 ]
 
 MIDDLEWARE = [
