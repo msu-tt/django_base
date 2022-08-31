5517ff78aaaf3b3acb93ce94b9208e1826c24db2
zyh <2508939001@qq.com>
2022-08-31 14:45:43 +0800

    create super user

--- bookmanager/book0/models.py
+++ bookmanager/book0/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    # id
+    name = models.CharField(max_length=10)
+
+# prople
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
